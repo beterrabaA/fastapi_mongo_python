b7389c6924588f97e60ddc13672cf7f638e37e86
Beterraba <ls6182315@gmail.com>
2024-07-04 23:00:52 -0300

    create mongo database config

--- config/database.py
+++ config/database.py
@@ -1,5 +1,10 @@
 from pymongo import MongoClient
+import os
 
-url = 'that is ok'
+url = os.environ.get('DATABASE_URL')
 
 client = MongoClient(url)
+
+db = client.todo_db
+
+collection_name = db['collection_todo']
