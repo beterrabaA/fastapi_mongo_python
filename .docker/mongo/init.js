21cf01643bac711441e3a05e2310ad02c227a300
Lucas Vieira <ls6182315@gmail.com>
2024-07-22 14:04:40 -0300

    remove preset data

--- .docker/mongo/init.js
+++ .docker/mongo/init.js
@@ -1,21 +1,3 @@
 db = db.getSiblingDB('todo_db');
 
 db.createCollection('tasks_collection');
-
-db.sample_collection.insertMany([
- {
-    name: 'helpdev',
-    description: 'salada mista',
-    is_done: false
-  },
-  {
-    name: 'helpdev',
-    description: 'quero quero',
-    is_done: false
-  },
-  {
-    name: 'helpdev',
-    description: 'passagem de onibus',
-    is_done: false
-  }  
-]);
